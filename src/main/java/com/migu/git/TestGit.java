17a5988842e35396d0f907295308a4b000bad8ac
775817199 <zmy775817199@sina.com>
2019-06-18 14:08:18 +0800

    第二次提交

---
+++ src/main/java/com/migu/git/TestGit.java
@@ -0,0 +1,12 @@
+package com.migu.git;
+
+/**
+ * @author zmy
+ * @email 775817199@qq.com
+ * @create 2019-06-18 14:02
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
